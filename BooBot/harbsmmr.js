a41349a8ff2cf8da28c2e9ef96b20391ed443835
Jack <jackbondpreston@outlook.com>
2016-06-30 00:12:47 +0100

    Reformatting, add Closure Compiler config
    Now using http://closure-compiler.appspot.com/ to compile js as it is more compact and allows us to use any length of variable/function names.

--- BooBot/harbsmmr.js
+++ BooBot/harbsmmr.js
@@ -1,120 +1,146 @@
-function pd_r(s, c, n) {
-  if(s.length>n)
-	  s = s.substr(0,n-4)+"...";
-  while(s.length<n-c.length)
-  {s+=c;}
-  return s;
+// ==ClosureCompiler==
+// @compilation_level ADVANCED_OPTIMIZATIONS
+// @output_file_name default.js
+// @js_externs server.Users; input; user.Roles; user.CurrentGame; user.State; user.Name; role.Name; role.Position;
+// ==/ClosureCompiler==
+
+function pad_right(str, c, len) {
+	if(str.length > len) {
+		str = str.substr(0, len - 4) + "...";
+	}
+	while(str.length < len - c.length) {
+		str += c;
+	}
+	return str;
 }
 
-var mrs =[];
-if(input!=""){var mrs = input.split(" ");}else{print("No input given, dumbass.")}
+var mmrs = [];
+if(input != "") {
+	mmrs = input.split(" ");
+} else {
+	print("No input given, friend.");
+}
+
 var users = [];
-for(var a  = 0; a<mrs.length;a++)
-{users[a] = [];}
-for(var a = 0; a<server.Users.length;a++)
-{
+for(var a  = 0; a < mmrs.length; a++) {
+	users[a] = [];
+}
+
+for(var a = 0; a < server.Users.length; a++) {
 	var user = server.Users[a];
-	var lowest = 0,mrSlot = -1;var state = -1;var game = -1;
-	for(var b = 0; b < user.Roles.length;b++)
-	{
+	var lowest = 0, mmrSlot = -1, state = -1, game = -1;
+	for(var b = 0; b < user.Roles.length; b++) {
 		var role = user.Roles[b];
-		for(var c = 0; c< mrs.length;c++)
-		{
-			if(role.Name==mrs[c])
-				mrSlot = c;
+		for(var c = 0; c < mmrs.length;c++) {
+			if(role.Name == mmrs[c]) {
+				mmrSlot = c;
+			}
 		}
-		if(role.Position<user.Roles[lowest])
+		if(role.Position < user.Roles[lowest]) {
 			lowest = b;
+		}
 	}
-	switch(user.State){
-		case "offline": state = 2;break;
-		case "idle": state = 1;break;
-		case "online": state = 0; break;
+	
+	switch(user.State) {
+		case "offline": 
+			state = 2;
+			break;
+		case "idle":
+			state = 1;
+			break;
+		case "online":
+			state = 0;
+			break;
 	}
-	var dspgm;
-	if(/dota ?2/i.test(user.CurrentGame))
-	{
-		dspgm = "in dota2";
+	
+	var displayGame;
+	if(/dota ?2/i.test(user.CurrentGame)) {
+		displayGame = "in dota2";
 		game = 0;
+	} else if(user.CurrentGame == null) {	
+		game = 1;
+		if(state == 2) {
+			displayGame="//offline";
+		} else {
+			displayGame="'"+user.State+"'";
+		}
+	} else {
+		displayGame = "'in another game'";
+		game = 2;
 	}
-	else
-		if(user.CurrentGame == null)
-		{	
-			game = 1;
-			if(state==2)
-				dspgm="//offline";
-			else
-				dspgm="'"+user.State+"'";
+	
+	if(mmrSlot >= 0) {
+		var displayName = user.Name.toUpperCase()
+		displayName = displayName.replace(/[\u0250-\ue007]/g, "").trim();
+		if(displayName.length < user.Name.Length-3) {
+			displayName = "<GIBBERISH>";
 		}
-		else
-		{
-			dspgm = "'in another game'";
-			game = 2;
-		}
-	if(mrSlot>=0)
-	{
-		var tmp1 = user.Name.toUpperCase()
-		tmp1 = tmp1.replace(/[\u0250-\ue007]/g, "").trim();
-		if(tmp1.length<user.Name.Length-3)
-			tmp1 = "<GIBBERISH>";
-		users[mrSlot].push([tmp1,user.Roles[lowest].Name,lowest,user.CurrentGame,game,state,dspgm]);
+		users[mmrSlot].push([displayName, user.Roles[lowest].Name, lowest, user.CurrentGame, game, state, displayGame]);
 	}
 }
-var ops=[];var totalLength = 0;
-for(var a = 0; a<mrs.length;a++)
-{
-	ops[a]=[];
+
+var ops = [], totalLength = 0;
+for(var a = 0; a < mmrs.length;a++) {
+	ops[a] = [];
 	users[a].sort(
-		function(a,b){
-			if(a[4]==b[4]){//game
-				if(a[5]==b[5]){
-					if(a[2]==b[2]){//role
-						if(String(a[0]).toLowerCase()<String(b[0]).toLowerCase())//namecheck
+		function(a, b) {
+			if(a[4] == b[4]) {//game
+				if(a[5] == b[5]) {
+					if(a[2] == b[2]) {//role
+						if(String(a[0]).toLowerCase() < String(b[0]).toLowerCase()) {//namecheck
 							return -1;
+						}
 						return 1;
 					}
-					return a[2]-b[2];
+					return a[2] - b[2];
 				}
-				return a[5]-b[5];
+				return a[5] - b[5];
 			}
-			if(b[5]==2)
+			if(b[5] == 2) {
 				return -1;
-			if(a[5]==2)
+			} else if(a[5] == 2) {
 				return 1;
-			return a[4]-b[4];
+			} else {
+				return a[4] - b[4];
+			}
 		}
 	);
-	var tmp = "";
-	if(a!=0)
-		tmp+="\n";
-	tmp += "__**"+mrs[a]+"**__```xl\n";
-	tmp+= pd_r("USER"," ",25)+"|"+pd_r("INGAME STATUS"," ",25)+"\n";
-	tmp+= pd_r("─","─",25)+"┼" + pd_r("─","─",25)+"\n";
 	
-	ops[a].push(tmp);ops[a].push([]);
-	for(var b = 0; b<users[a].length;b++)
-	{
+	var temp = "";
+	if(a != 0) {
+		temp += "\n";
+	}
+	
+	temp += "__**" + mmrs[a] + "**__```xl\n";
+	temp += pad_right("USER", " ", 25) + "|" + pad_right("INGAME STATUS", " ", 25) + "\n";
+	temp += pad_right("─", "─", 25) + "┼" + pad_right("─", "─", 25) + "\n";
+	
+	ops[a].push(temp);
+	ops[a].push([]);
+	for(var b = 0; b < users[a].length; b++) {
 		user = users[a][b];
-		ops[a][1].push(pd_r(user[0]," ",25)+"│"+user[6]+"\n");
-		totalLength+=26+user[6].length;
+		ops[a][1].push(pad_right(user[0], " ", 25) + "│" + user[6] + "\n");
+		totalLength += 26 + user[6].length;
 	}
 	totalLength+=126;
 }
 
-var op= "";var opSize,spaceRemaining;
-if(totalLength<2000)
+var op = "";
+var opSize, spaceRemaining;
+
+if(totalLength < 2000) {
 	opSize = 3000;
-else
-	opSize = (2000-(mrs.length*126))/mrs.length;
-for(var a=0;a<ops.length;a++)
-{
-	op+= ops[a][0];
+} else {
+	opSize = (2000 - (mmrs.length * 126)) / mmrs.length;
+}
+
+for(var a = 0; a < ops.length; a++) {
+	op += ops[a][0];
 	spaceRemaining = opSize;
-	for(var b=0;b<ops[a][1].length&&spaceRemaining > 53;b++)
-	{
-		spaceRemaining-=ops[a][1][b].length;
-		op+=ops[a][1][b];
+	for(var b = 0; b < ops[a][1].length && spaceRemaining > 53; b++) {
+		spaceRemaining -= ops[a][1][b].length;
+		op += ops[a][1][b];
 	}
-	op+="```";
+	op += "```";
 }
 print(op);
