03646a1befcca20c69ab328df56a7f2a06312010
Guido "Harb <Harbingerofme@gmail.com>
2016-07-13 01:53:01 +0200

    Example of multiple quotes for single user
    only 3 lines need editing, for each user. hoxieloxie was chosen as the
    example as she was the first this was brought up on. Bound to be
    deprecated by trivia subproject.

---
+++ BooBot/hoxie.js
@@ -0,0 +1,45 @@
+function isInt(value) {
+  var x;
+  if (isNaN(value)) {
+    return false;
+  }
+  x = parseFloat(value);
+  return (x | 0) === x;
+}
+//if copying this command, change these lines:
+var quotes = ["http://i.imgur.com/5uMiR0E.png","https://cdn.discordapp.com/attachments/143755095895572481/195968925060562947/Screenshot_607.png","​http://i.imgur.com/praiWNh.png","​http://i.imgur.com/lkSHylD.png"];
+var quotesText = ["i'm a thirsty hoe","AND THEN HE LANDED ME THE GREATEST BITCH OF THEM ALL","i eat ass","Smash that pasty"];
+var thisCommand = "~hoxie"; var userName = "hoxieloxie";
+//stop changing from here.
+var quote = "Quote not found.";
+switch(input.substring(0,2))
+	{
+	case "": case "-h":
+		quote = "I will respond with a quote from "+userName+", if you give me some words from that quote or a number. I currently have "+quotes.length+" quotes stored. Examples: `"+thisCommand+" 2`, `"+thisCommand+" landed`. Type `"+thisCommand+" -all` to see a text version of all available quotes. Type `"+thisCommand+" -random` to get a random one.";
+	break;
+	
+	case "-a":
+		quote = "Printing text versions of all quotes: ```";
+		for(var a = 0; a<quotes.length;a++){
+			quote+="\n"+quotesText[a];
+		}
+		quote += "```";
+	break;
+	
+	case "-r":
+		quote = quotes[random(quotes.length)];
+	break;
+	
+	default:
+		if(isInt(input))
+				quote = quotes[input-1];
+			
+		if(quote == "Quote not found."){
+				for(var a = 0; a<quotes.length; a++){
+					if(quotesText[a].toLowerCase().indexOf(input.toLowerCase()) > -1)
+						quote = quotes[a];
+			}
+		}
+	break;
+	}
+print(quote);
